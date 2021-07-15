18ab234548bbf3bf150f2ec4094d2d50d3b0d6cf
Andrés <aac.devs@gmail.com>
2021-07-15 02:28:59 -0300

    section 25 finished

--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,17 +1,17 @@
-import React, { useContext } from "react";
+import React from "react";
 
 import ProductItem from "../components/Products/ProductItem";
+import { useStore } from "../hooks-store/store";
 import "./Products.css";
-import { ProductsContext } from "../context/products-context";
 
 const Products = (props) => {
-  const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
 
-  console.log(productList);
+  console.log(state);
 
   return (
     <ul className="products-list">
-      {productList.map((prod) => (
+      {state.products.map((prod) => (
         <ProductItem
           key={prod.id}
           id={prod.id}
